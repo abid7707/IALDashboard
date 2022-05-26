29dff64bca251bef30eba1d1bce992b79655680c
abid7707 <abid.it@ifadgroup.com>
2022-05-26 18:07:44 +0600

    add user authentication

---
+++ IALDashboard/Controllers/UserController.cs
@@ -0,0 +1,181 @@
+﻿using IALDashboard.DAL;
+using IALDashboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IALDashboard.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            if (Session["isLogedin"] != null)
+            {
+                return RedirectToAction("Dashboard", "Home");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Verify(User user)
+        {
+            DataTable dt = new User_DAL().VerifyUser(user);
+
+            //DataTable dt = new db().getUserInfo(user);
+
+            if (dt.Rows.Count > 0)
+            {
+
+                DataRow dr = dt.Rows[0];
+                Session["user_id"] = Convert.ToString(dr["USER_ID"]);
+                Session["user_name"] = Convert.ToString(dr["USER_NAME"]);
+                Session["user_area"] = Convert.ToString(dr["USER_AREA"]);
+                Session["isLogedin"] = true;
+
+                DataTable menuAccess = new User_DAL().UserMenuAccess(Convert.ToString(dr["USER_ID"]));
+                Session["menuAccess"] = menuAccess;
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.error = "User ID or Password is not correct";
+                return View("Login");
+            }
+
+        }
+
+        public ActionResult Logout()
+        {
+
+            Session["isLogedin"] = null;
+            return RedirectToAction("Login", "User");
+        }
+
+
+        [HttpGet]
+        public string user_info(string user_id)
+        {
+            DataTable dt = new User_DAL().UserInfo(user_id);
+            //DataTable dt = new db().getUserInfo(user);
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(dt);
+            return json;
+
+        }
+
+
+        [Filters.AuthorizedUser]
+        public ActionResult Create()
+        {
+            DataTable menu = new User_DAL().getMenu("");
+            ViewBag.usermenu = menu;
+            return View();
+        }
+
+
+        [Filters.AuthorizedUser]
+        public ActionResult Save(FormCollection f)
+        {
+
+            string user_id = f["USER_ID"];
+            string USER_NAME = f["USER_NAME"];
+            string USER_EMAIL = f["USER_EMAIL"];
+            string USER_PASS = f["USER_PASS"];
+            string USER_AREA = f["USER_AREA"];
+
+            int result = new User_DAL().user_save(user_id, USER_NAME, USER_PASS, USER_EMAIL, USER_AREA);
+            if (result != 0)
+            {
+                if (f["MENU_ID"] != "")
+                {
+                    string[] MENUS = f["MENU_ID"].Split(',');
+                    foreach (string item in MENUS)
+                    {
+                        int MENU_ID = int.Parse(item);
+                        new User_DAL().menu_access_save(user_id, MENU_ID);
+                    }
+                }
+                ViewBag.save_message = "User id " + user_id + " created successfully";
+            }
+            else
+            {
+                ViewBag.save_message = "User id " + user_id + " has not created. Please try again.";
+            }
+            DataTable menu = new User_DAL().getMenu("");
+            ViewBag.usermenu = menu;
+
+            return View("Create");
+        }
+
+
+        [Filters.AuthorizedUser]
+        public ActionResult UserList()
+        {
+            DataTable dt = new User_DAL().UserInfo("");
+            ViewBag.userlist = dt;
+            return View();
+        }
+
+
+
+        [Filters.AuthorizedUser]
+        public ActionResult EditUser(string user_id)
+        {
+
+            DataTable dt = new User_DAL().UserInfo(user_id);
+            ViewBag.userinfo = dt;
+
+            DataTable menu = new User_DAL().getMenu(user_id);
+            ViewBag.usermenu = menu;
+
+            return View();
+        }
+
+
+        [Filters.AuthorizedUser]
+        public ActionResult UserUpdate(FormCollection f)
+        {
+
+            string user_id = f["USER_ID"];
+            string USER_NAME = f["USER_NAME"];
+            string USER_EMAIL = f["USER_EMAIL"];
+            string USER_AREA = f["USER_AREA"];
+
+            int result = new User_DAL().user_update(user_id, USER_NAME, USER_EMAIL, USER_AREA);
+            if (result != 0)
+            {
+                new User_DAL().menu_access_delete(user_id);
+
+                if (f["MENU_ID"] != "")
+                {
+                    string[] MENUS = f["MENU_ID"].Split(',');
+                    foreach (string item in MENUS)
+                    {
+                        int MENU_ID = int.Parse(item);
+                        new User_DAL().menu_access_save(user_id, MENU_ID);
+                    }
+                }
+                ViewBag.save_message = "User id " + user_id + " Update successfully";
+            }
+            else
+            {
+                ViewBag.save_message = "User id " + user_id + " has not updated. Please try again.";
+            }
+            DataTable menu = new User_DAL().getMenu("");
+            ViewBag.usermenu = menu;
+
+            return View("Create");
+        }
+    }
+}
---
+++ IALDashboard/DAL/User_DAL.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.OracleClient;
+using System.Configuration;
+using IALDashboard.Models;
+
+namespace IALDashboard.DAL
+{
+    public class User_DAL : BaseDataAccessLayer
+    {
+
+        public DataTable VerifyUser(User user)
+        {
+
+            DataTable userinfo = new DataTable();
+
+            try
+            {
+                string userid = user.user_id;
+                string password = MD5(user.password);
+                string StrSql = " SELECT USER_ID, USER_NAME, USER_PASS, USER_EMAIL, USER_AREA, USER_LEVEL FROM USER_TAB WHERE upper(user_id)=upper('" + userid + "') AND user_pass='" + password + "'";
+                OracleCommand command = GetSQLCommand(StrSql);
+                OracleDataAdapter odaAdapter = new OracleDataAdapter(command);
+                odaAdapter.Fill(userinfo);
+            }
+
+            catch
+            {
+            }
+
+            finally
+            {
+                Dispose();
+            }
+
+            return userinfo;
+        }
+
+        public DataTable UserInfo(string userid)
+        {
+
+            DataTable userinfo = new DataTable();
+
+            string condition = "";
+            if (userid != "") {
+                condition = " WHERE user_id='" + userid + "'";
+            }
+
+            try
+            {
+
+                string StrSql = " SELECT USER_ID, USER_NAME, USER_PASS, USER_EMAIL, USER_AREA, USER_LEVEL FROM USER_TAB "+condition;
+                OracleCommand command = GetSQLCommand(StrSql);
+                OracleDataAdapter odaAdapter = new OracleDataAdapter(command);
+                odaAdapter.Fill(userinfo);
+            }
+
+            catch
+            {
+            }
+
+            finally
+            {
+                Dispose();
+            }
+
+            return userinfo;
+        }
+
+        public DataTable UserMenuAccess(string user_id)
+        {
+            DataTable usermenu = new DataTable();
+            try
+            {
+
+                string StrSql = @"SELECT a.USER_ID,a.MENU_ID,m.MENU_LABEL,m.MENU_LINK,m.MENU_PARENT,m.SEQ FROM MENU_ACCESS_TAB a left outer join MENU_TAB m on a.MENU_ID=m.MENU_ID WHERE a.user_id='" + user_id + "' order by m.seq";
+                OracleCommand command = GetSQLCommand(StrSql);
+                OracleDataAdapter odaAdapter = new OracleDataAdapter(command);
+                odaAdapter.Fill(usermenu);
+            }
+
+            catch
+            {
+            }
+
+            finally
+            {
+                Dispose();
+            }
+
+            return usermenu;
+        }
+
+
+        public DataTable getMenu(string user_id)
+        {
+            DataTable usermenu = new DataTable();
+            try
+            {
+
+                string StrSql = @"SELECT a.USER_ID,m.MENU_ID,m.MENU_LABEL,m.MENU_LINK,m.MENU_PARENT,m.SEQ FROM MENU_TAB m left outer join MENU_ACCESS_TAB a  on a.MENU_ID=m.MENU_ID and a.user_id='" + user_id + "' order by m.seq";
+                OracleCommand command = GetSQLCommand(StrSql);
+                OracleDataAdapter odaAdapter = new OracleDataAdapter(command);
+                odaAdapter.Fill(usermenu);
+            }
+
+            catch
+            {
+            }
+
+            finally
+            {
+                Dispose();
+            }
+
+            return usermenu;
+
+        }
+
+      
+
+        public int user_save(string USER_ID, string USER_NAME, string Password, string USER_EMAIL = "", string USER_AREA="")
+        {
+
+            int result = 0;
+            string password = MD5(Password);
+
+            try
+            {
+
+                string StrSql = "INSERT INTO USER_TAB (USER_ID, USER_NAME, USER_PASS,USER_EMAIL,USER_AREA) VALUES('" + USER_ID + "','" + USER_NAME + "','" + password + "','" + USER_EMAIL + "','" + USER_AREA + "')";
+                OracleCommand command =  GetSPCommand(StrSql);
+                result = ExecuteCommand(command);
+
+            }
+            catch
+            {
+            }
+            finally
+            {
+                Dispose();
+            }
+            return result;
+
+        }
+
+        public int user_update(string USER_ID, string USER_NAME, string USER_EMAIL = "", string USER_AREA="")
+        {
+            int result = 0;
+            //string password = MD5(Password);
+            try
+            {
+                string StrSql = "UPDATE USER_TAB SET  USER_NAME='" + USER_NAME + "',USER_EMAIL='" + USER_EMAIL + "', USER_AREA='" + USER_AREA + "' WHERE USER_ID='" + USER_ID + "'";
+                OracleCommand command = GetSPCommand(StrSql);
+                result = ExecuteCommand(command);
+            }
+            catch
+            {
+            }
+            finally
+            {
+                Dispose();
+            }
+            return result;
+
+        }
+
+        public int menu_access_save(string USER_ID, int MENU_ID)
+        {
+
+            int result = 0;
+
+            try
+            {
+
+                string StrSql = "INSERT INTO MENU_ACCESS_TAB (USER_ID, MENU_ID) VALUES('"+USER_ID+"',"+MENU_ID+")";
+                OracleCommand command = GetSPCommand(StrSql);
+
+                result = ExecuteCommand(command);
+            }
+            catch
+            {
+            }
+            finally
+            {
+                Dispose();
+            }
+            return result;
+
+        }
+
+        public int menu_access_delete(string USER_ID)
+        {
+            int result = 0;
+            try
+            {
+                string StrSql = "DELETE FROM MENU_ACCESS_TAB WHERE USER_ID='" + USER_ID + "'";
+                OracleCommand command = GetSPCommand(StrSql);
+
+                result = ExecuteCommand(command);
+            }
+            catch
+            {
+            }
+            finally
+            {
+                Dispose();
+            }
+            return result;
+
+        }
+
+
+
+        private string MD5(string Metin)
+        {
+            MD5CryptoServiceProvider MD5Code = new MD5CryptoServiceProvider();
+            byte[] byteDizisi = Encoding.UTF8.GetBytes(Metin);
+            byteDizisi = MD5Code.ComputeHash(byteDizisi);
+            StringBuilder sb = new StringBuilder();
+            foreach (byte ba in byteDizisi)
+            {
+                sb.Append(ba.ToString("x2").ToLower());
+            }
+            return sb.ToString();
+        }
+    }
+}
