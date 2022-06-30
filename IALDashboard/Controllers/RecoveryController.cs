1b2dc95f1fcb9376714c0f4d63961f33e8e2da9e
Hira Sharker <hira.it@ifadgroup.com>
2022-06-30 11:56:47 +0600

    modify RO Sheet

--- IALDashboard/Controllers/RecoveryController.cs
+++ IALDashboard/Controllers/RecoveryController.cs
@@ -1,6 +1,7 @@
 ﻿using ClosedXML.Excel;
 using IALDashboard.DAL;
 using Newtonsoft.Json;
+using System;
 using System.Data;
 using System.IO;
 using System.Web.Mvc;
@@ -59,97 +60,145 @@ namespace IALDashboard.Controllers
         {
             from_date = from_date + "-01";
 
-            DataTable dt = new Collection_DAL().ROSheet(from_date, ro_code, zone_name);
-            DataTable dt_ro = new Collection_DAL().getROByCODE(ro_code);
-            DataRow ro_row = dt_ro.Rows[0];
+            DataTable ro_list = new Collection_DAL().ROListByZone(from_date, zone_name);
 
+            DataTable dt = new Collection_DAL().ROSheet(from_date, ro_code, zone_name);
+
+
+
+            /*DataTable dt_ro = new Collection_DAL().getROByCODE(ro_code);
+            DataRow ro_row = dt_ro.Rows[0];*/
             using (XLWorkbook wb = new XLWorkbook())
             {
-                var ws = wb.Worksheets.Add("ROSheet");
-
-                ws.Range("A1:P5").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
-                ws.Cell("A1").Value = "Monthly Target Sheet ";
-                ws.Range("A1:P1").Merge().Style.Font.SetBold().Font.FontSize = 14;
-
-                ws.Cell("A2").Value = "Month: " + from_date;
-                ws.Range("A2:P2").Merge().Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("A3").Value = "RO CODE: " + ro_row["RO_CODE"] + "RO Name: " + ro_row["RO_NAME"] + " ZONE: " + zone_name;
-                ws.Range("A3:P3").Merge().Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("A5").Value = "Sl NO";
-                ws.Range("A5").Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("B5").Value = "Order No";
-                ws.Range("B5").Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("C5").Value = "Customer Name";
-                ws.Range("C5").Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("D5").Value = "Registration No";
-                ws.Range("D5").Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("E5").Value = "Model Name";
-                ws.Range("E5").Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("F5").Value = "1st EMI DATE";
-                ws.Range("F5").Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("G5").Value = "EMI AMT.";
-                ws.Range("G5").Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("H5").Value = "Target INST AMT.";
-                ws.Range("H5").Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("I5").Value = "NO of Overdue";
-                ws.Range("I5").Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("J5").Value = "MR COLL";
-                ws.Range("J5").Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("K5").Value = "DP+DC Due";
-                ws.Range("K5").Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("L5").Value = "INST DC";
-                ws.Range("L5").Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("M5").Value = "Overdue";
-                ws.Range("M5").Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("N5").Value = "Monthly Coll";
-                ws.Range("N5").Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("O5").Value = "Inst. Coll";
-                ws.Range("O5").Style.Font.SetBold().Font.FontSize = 12;
-
-                ws.Cell("P5").Value = "Atten V";
-                ws.Range("P5").Style.Font.SetBold().Font.FontSize = 12;
-
-
-                if (dt.Rows.Count > 0)
+                foreach (DataRow row in ro_list.Rows)
                 {
-                    for (int i = 0; i < dt.Rows.Count; i++)
+                    var ws = wb.Worksheets.Add(row["RO_NAME"].ToString());
+
+
+
+                    ws.Range("A1:P5").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+                    ws.Cell("A1").Value = "Monthly Target Sheet ";
+                    ws.Range("A1:P1").Merge().Style.Font.SetBold().Font.FontSize = 14;
+
+                    ws.Cell("A2").Value = "Month: " + from_date;
+                    ws.Range("A2:P2").Merge().Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("A3").Value = "RO CODE: " + row["RO_CODE"].ToString() + "RO Name: " + row["RO_NAME"].ToString() + " ZONE: " + zone_name;
+                    ws.Range("A3:P3").Merge().Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("A5").Value = "Sl NO";
+                    ws.Range("A5").Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("B5").Value = "Order No";
+                    ws.Range("B5").Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("C5").Value = "Customer Name";
+                    ws.Range("C5").Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("D5").Value = "Registration No";
+                    ws.Range("D5").Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("E5").Value = "Model Name";
+                    ws.Range("E5").Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("F5").Value = "1st EMI DATE";
+                    ws.Range("F5").Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("G5").Value = "EMI AMT.";
+                    ws.Range("G5").Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("H5").Value = "Target INST AMT.";
+                    ws.Range("H5").Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("I5").Value = "NO of Overdue";
+                    ws.Range("I5").Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("J5").Value = "MR COLL";
+                    ws.Range("J5").Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("K5").Value = "DP+DC Due";
+                    ws.Range("K5").Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("L5").Value = "INST DC";
+                    ws.Range("L5").Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("M5").Value = "Overdue";
+                    ws.Range("M5").Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("N5").Value = "Monthly Coll";
+                    ws.Range("N5").Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("O5").Value = "Inst. Coll";
+                    ws.Range("O5").Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Cell("P5").Value = "Atten V";
+                    ws.Range("P5").Style.Font.SetBold().Font.FontSize = 12;
+
+                    DataRow[] rodt = dt.Select("RO_CODE =" + row["RO_CODE"]);
+                    int i = 0;
+                    double SUB_EMI_AMOUNT = 0;
+                    double SUB_TAR_INST_AMT = 0;
+                    double SUB_NO_OF_OVERDUE = 0;
+                    double SUB_MR_COLL = 0;
+                    double SUB_DUE_DP_AND_DC = 0;
+                    double SUB_INS_DC_PAYMENT = 0;
+                    double SUB_OVERDUE = 0;
+                    double SUB_MONTHLY_COLL = 0;
+                    double SUB_INST_COLL = 0;
+                    double SUB_ATTEN_V = 0;
+
+                    foreach (DataRow record in rodt)
                     {
+                        SUB_EMI_AMOUNT += Convert.ToDouble(record["EMI_AMOUNT"]);
+                        SUB_TAR_INST_AMT += Convert.ToDouble(record["TAR_INST_AMT"]);
+                        SUB_NO_OF_OVERDUE += Convert.ToDouble(record["NO_OF_OVERDUE"]);
+                        SUB_MR_COLL += Convert.ToDouble(record["MR_COLL"]);
+                        SUB_DUE_DP_AND_DC += Convert.ToDouble(record["DUE_DP_AND_DC"]);
+                        SUB_INS_DC_PAYMENT += Convert.ToDouble(record["INS_DC_PAYMENT"]);
+                        SUB_OVERDUE += Convert.ToDouble(record["OVERDUE"]);
+                        SUB_MONTHLY_COLL += Convert.ToDouble(record["MONTHLY_COLL"]);
+                        SUB_INST_COLL += Convert.ToDouble(record["INST_COLL"]);
+                        SUB_ATTEN_V += Convert.ToDouble(record["ATTEN_V"]);
+
                         ws.Cell("A" + (i + 6)).Value = i + 1;
-                        ws.Cell("B" + (i + 6)).Value = dt.Rows[i]["ORDER_NO"];
-                        ws.Cell("C" + (i + 6)).Value = dt.Rows[i]["CUSTOMER_NAME"];
-                        ws.Cell("D" + (i + 6)).Value = dt.Rows[i]["REGNO"];
-                        ws.Cell("E" + (i + 6)).Value = dt.Rows[i]["CATALOG_DESC"];
-                        ws.Cell("F" + (i + 6)).Value = dt.Rows[i]["FSTINSAL_DATE"];
-                        ws.Cell("G" + (i + 6)).Value = dt.Rows[i]["EMI_AMOUNT"];
-                        ws.Cell("H" + (i + 6)).Value = dt.Rows[i]["TAR_INST_AMT"];
-                        ws.Cell("I" + (i + 6)).Value = dt.Rows[i]["NO_OF_OVERDUE"];
-                        ws.Cell("J" + (i + 6)).Value = dt.Rows[i]["MR_COLL"];
-                        ws.Cell("K" + (i + 6)).Value = dt.Rows[i]["DP_DC_PAYMENT"];
-                        ws.Cell("L" + (i + 6)).Value = dt.Rows[i]["INS_DC_PAYMENT"];
-                        ws.Cell("M" + (i + 6)).Value = dt.Rows[i]["OVERDUE"];
-                        ws.Cell("N" + (i + 6)).Value = dt.Rows[i]["MONTHLY_COLL"];
-                        ws.Cell("O" + (i + 6)).Value = dt.Rows[i]["INST_COLL"];
-                        ws.Cell("P" + (i + 6)).Value = dt.Rows[i]["ATTEN_V"];
+                        ws.Cell("B" + (i + 6)).Value = record["ORDER_NO"].ToString();
+                        ws.Cell("C" + (i + 6)).Value = record["CUSTOMER_NAME"].ToString();
+                        ws.Cell("D" + (i + 6)).Value = record["REGNO"].ToString();
+                        ws.Cell("E" + (i + 6)).Value = record["CATALOG_DESC"].ToString();
+                        ws.Cell("F" + (i + 6)).Value = record["FSTINSAL_DATE"].ToString();
+                        ws.Cell("G" + (i + 6)).Value = record["EMI_AMOUNT"].ToString();
+                        ws.Cell("H" + (i + 6)).Value = record["TAR_INST_AMT"].ToString();
+                        ws.Cell("I" + (i + 6)).Value = record["NO_OF_OVERDUE"].ToString();
+                        ws.Cell("J" + (i + 6)).Value = record["MR_COLL"].ToString();
+                        ws.Cell("K" + (i + 6)).Value = record["DUE_DP_AND_DC"].ToString();
+                        ws.Cell("L" + (i + 6)).Value = record["INS_DC_PAYMENT"].ToString();
+                        ws.Cell("M" + (i + 6)).Value = record["OVERDUE"].ToString();
+                        ws.Cell("N" + (i + 6)).Value = record["MONTHLY_COLL"].ToString();
+                        ws.Cell("O" + (i + 6)).Value = record["INST_COLL"].ToString();
+                        ws.Cell("P" + (i + 6)).Value = record["ATTEN_V"].ToString();
+                        i++;
                     }
+                    ws.Cell("F" + (i + 6)).Value = "Sub Total";
+                    ws.Cell("G" + (i + 6)).Value = SUB_EMI_AMOUNT.ToString();
+                    ws.Cell("H" + (i + 6)).Value = SUB_TAR_INST_AMT.ToString();
+                    ws.Cell("I" + (i + 6)).Value = SUB_NO_OF_OVERDUE.ToString();
+                    ws.Cell("J" + (i + 6)).Value = SUB_MR_COLL.ToString();
+                    ws.Cell("K" + (i + 6)).Value = SUB_DUE_DP_AND_DC.ToString();
+                    ws.Cell("L" + (i + 6)).Value = SUB_INS_DC_PAYMENT.ToString();
+                    ws.Cell("M" + (i + 6)).Value = SUB_OVERDUE.ToString();
+                    ws.Cell("N" + (i + 6)).Value = SUB_MONTHLY_COLL.ToString();
+                    ws.Cell("O" + (i + 6)).Value = SUB_INST_COLL.ToString();
+                    ws.Cell("P" + (i + 6)).Value = SUB_ATTEN_V.ToString();
+
+                    ws.Range("F" + (i + 6) + ":P" + (i + 6)).Style.Font.SetBold().Font.FontSize = 12;
+
+                    ws.Range("A5" + ":P" + (i + 6)).Style.Alignment.SetWrapText(true);
+
+                    ws.PageSetup.PageOrientation = XLPageOrientation.Landscape;
+                    ws.PageSetup.AdjustTo(80);
+                    ws.PageSetup.SetRowsToRepeatAtTop(1, 5);
 
                 }
-
                 using (MemoryStream stream = new MemoryStream())
                 {
                     wb.SaveAs(stream);
